a3d585e81c565b670468b7ffadf2b01a85df273a
João Winckler <jpwbernardi@hotmail.com>
2016-03-21 13:27:26 -0300

    uri1587 - Código melhorado

--- uri1587.cpp
+++ uri1587.cpp
@@ -8,76 +8,74 @@ int J;
 long long jA[MAX], jB[MAX];
 long long pA[MAX], pB[MAX];
 
-void incrementa(char t, int i, int v){
+void atualizaA(int i, int v){
     for(; i < MAX; i += (i & (-i)))
-        if(t == 'a') pA[i] += v;
-        else pB[i] += v;
+        pA[i] += v;
 }
 
-int get(char t, int i){
+void atualizaB(int i, int v){
+    for(; i < MAX; i += (i & (-i)))
+        pB[i] += v;
+}
+
+int qtdJogadoresA(int i){
     int soma = 0;
     for(; i > 0; i -= (i & (-i)))
-        if(t == 'a') soma += pA[i];
-        else soma += pB[i];
+        soma += pA[i];
     return soma;
+}
+
+int qtdJogadoresB(int i){
+    int soma = 0;
+    for(; i > 0; i -= (i & (-i)))
+        soma += pB[i];
+    return soma;
+}
+
+int posicaoValidaA(int i){
+    return (11 * (J - qtdJogadoresB(jA[i])) < N);
+}
+
+int posicaoValidaB(int i){
+    return (11 * qtdJogadoresA(jB[i] - 1) < N);
 }
 
 int main(void){
     char ev;
     int i, tmp, imp, gA, gB;
     while(scanf("%d %d", &N, &E), N){
-        gA = gB = imp = 0;
-        memset(pA, 0, sizeof(pA));
-        memset(pB, 0, sizeof(pB));
-        J = N / 2;
+        gA = gB = imp = 0; J = N / 2;
+        memset(pA, 0, sizeof(pA)); memset(pB, 0, sizeof(pB));
         for(i = 1; i <= J; i++){
-            scanf("%d", &tmp);
-            jA[i] = tmp + 1;
-            incrementa('a', jA[i], 1);
+            scanf("%d", &tmp); jA[i] = tmp + 1;
+            atualizaA(jA[i], 1);
         }
-        for(i = J + 1; i <= N; i++){
-            scanf("%d", &tmp);
-            jB[i] = tmp + 1;
-            incrementa('b', jB[i], 1);
+        for(i = 1; i <= J; i++){
+            scanf("%d", &tmp); jB[i] = tmp + 1;
+            atualizaB(jB[i], 1);
         }
         while(E--){
             scanf(" %c", &ev);
-            if(ev == 'S'){
-                imp = 0;
-            }else if(ev == 'G'){
+            if(ev == 'S') imp = 0;
+            else if(ev == 'G'){
                 scanf(" %c", &ev);
-                if (!imp){
-                    if(ev == 'A') gB++;
-                    else gA++;
-                }
-                imp = 0;
-            }else if(ev == 'P' || ev == 'I'){
+                if(imp){ imp = 0; continue; }
+                if(ev == 'A') gB++;
+                else gA++;
+            }else if(ev == 'I' || ev == 'P'){
                 scanf(" %c%d", &ev, &tmp);
-                if(ev == 'A') {
-                    //Conta quantos jogadores do time B tem até a posição do jogar A, mas como quero saber os jogadores que estão depois disso, subtraio de J
-                    imp |= (11 * (J - get('b', jA[tmp])) < N);
-                } else {
-
-                    // printf("Jtmp: %d, get: %d\n", tmp + J, get('a', jB[tmp + J] - 1));
-                    //Conta quantos jogadores do time A encontramos antes do jogador B
-                    imp |= (11 * get('a', jB[tmp + J] - 1) < N);
-                }
-            }else{
+                if(ev == 'A') imp |= posicaoValidaA(tmp);
+                else imp |= posicaoValidaB(tmp);
+            } else {
                 scanf(" %c%d %d", &ev, &tmp, &i);
-                //Tiro o cara da posição dele
-                incrementa(ev, ev == 'A' ? jA[tmp] : jB[tmp + J], -1);
-                //Coloco na posição nova
-                incrementa(ev, i + 1, 1);
-                //Guardo a posição
-                if(ev == 'A') jA[tmp] = i + 1;
-                else jB[tmp] = i + 1;
-            }
-        }
+                if(ev == 'A'){
+                    atualizaA(jA[tmp], -1);
+                    atualizaA((jA[tmp] = i + 1), 1);
+                }else{
+                    atualizaB(jB[tmp], -1);
+                    atualizaB((jB[tmp] = i + 1), 1);
+                }}}
         printf("%d X %d\n", gA, gB);
     }
     return 0;
 }
-
-
-//Créditos: Matheus Dall Rosa :P
-//Código ficou muito feio
